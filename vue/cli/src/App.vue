e11b1d81ac2983ad9e23020ffb17863f4c6ecc9c
Fernando Dias <fernando.dias@sicoob.com.br>
2024-03-12 11:11:42 -0300

    Aula de cli do Vue e criação do projeto

---
+++ vue/cli/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <home-component/>
+
+  </div>
+
+
+</template>
+
+<script>
+
+import HomeComponent from './components/HomeComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HomeComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/cli/src/components/HomeComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <footer id="footer">
+            <p>Bem Vindo: {{ nome }} Olá mundo do Vue com CLI</p>
+        </footer>
+    </div>
+</template>
+
+<script>
+    export default {
+        name : "HomeComponent",
+        data() {
+            return {
+                nome : "Fernando"
+            }
+        }
+    }
+
+
+</script>
