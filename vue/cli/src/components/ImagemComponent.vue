cad1140535fb610c23067b8d82054bce6598940f
Ana Luisa Caixeta <02287959661@dccbra.ifb.local>
2024-04-08 20:49:51 -0300

    Aula dia 08/04/2024

--- vue/cli/src/components/ImagemComponent.vue
+++ vue/cli/src/components/ImagemComponent.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="container">
-        <img :src="urlImage" :alt="altImage"/>
+        <p>Nome da Imagem: {{ nomeImagem }}</p>
+        <img v-show="imagemvVisivel" :src="urlImage" :alt="altImage"/>
+        <button @click="apareceEsconde(), segundoMetodo()">Aparece/Esconde</button>
     </div>
 </template>
 
@@ -10,13 +12,56 @@
         data() {
             return {
                 altImage: "Banner da Imagem",
-                urlImage: "https://blog.ipleaders.in/wp-content/uploads/2021/08/law-firm-1.jpg"
+                //urlImage: "https://blog.ipleaders.in/wp-content/uploads/2021/08/law-firm-1.jpg",
+                imagemvVisivel: false
             }
-        }
+        },
+        methods: {
+            apareceEsconde() {
+                this.imagemvVisivel = !this.imagemvVisivel;
+                this.$emit('visibilidadeImg', this.imagemvVisivel);
+            },
+            segundoMetodo (){
+                setTimeout(function(){
+                    console.log("Segundo Método Chamado")
+                }, 2000);
+            }
+        },
+        props: {
+            nomeImagem: String, 
+            urlImage: String
+        },
+        emits: ["visibilidadeImg"]
     }
 </script>
 
 <style scoped>
+    button {
+        padding: 12px;
+        font-weight: bold;
+        color: darkcyan;
+        background-color: azure;
+        border: solid 2px darkcyan;
+        border-radius: 8px;
+        transition: 0.5s;
+        cursor: pointer;
+    }
+
+    button:hover{
+        background: darkcyan;
+        color: azure;
+    }
+
+    .container {
+        display: flex;
+        flex-direction: column;
+        widows: 50%;
+        align-items: center;
+        margin: 16px;
+        border-bottom: solid 2px lightblue;
+        padding: 16px;
+    }
+
     img {
         width: 300px;
         height: auto;
--- vue/cli/src/components/UsuarioComponent.vue
+++ vue/cli/src/components/UsuarioComponent.vue
@@ -1,9 +1,14 @@
 <template>
     <div>
           <p>Nome: {{ nome }}</p>
-          <p>E-mail: {{ email }}</p>  
-          <imagem-component/>
+          <p>E-mail: {{ email }}</p>
+
+          <h1 v-show="isNetlixMelhor"> Netflix é melhor</h1>
+
+          <imagem-component @visibilidadeImg="exibirMensagemNetflix" nomeImagem="Netflix" urlImage="https://about.netflix.com/images/meta/netflix-symbol-black.png"/>
+          <imagem-component nomeImagem="HBO Max" urlImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTek8mCpf41kM0vQ-VJav-NY16XlEF4L-UMlWinuX4Lqg&s"/>
     </div>
+
 </template>
 
 <script>
@@ -17,7 +22,18 @@
         data(){
             return {
                 nome : "Ana Luísa",
-                email : "anca.caixeta@estudante.ifb.edu.br"
+                email : "ana.caixeta@estudante.ifb.edu.br",
+                isNetlixMelhor: false
+            }
+        },
+        methods: {
+            exibirMensagemNetflix(isImagemVisivel){
+                if(isImagemVisivel === true){
+                    this.isNetlixMelhor = true;
+                }
+                else{
+                    this.isNetlixMelhor = false;
+                }
             }
         }
     }
