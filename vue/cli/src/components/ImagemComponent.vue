401a18c2e78e6ec70b50cd719cdd9d578b473a3d
Maycon Douglas Nunes De Almeida <05980663142@dccbra.ifb.local>
2024-04-08 21:00:24 -0300

    aula 0/04

--- vue/cli/src/components/ImagemComponent.vue
+++ vue/cli/src/components/ImagemComponent.vue
@@ -1,32 +1,69 @@
 <template>
-    <div class="container">
-        <img :src="urlImagem" :alt="altImagem">
-    </div>
+  <div class="container">
+    <p>Nome da image: {{ nomeImagem }}</p>
+    <img v-show="imageVisivel" :src="urlImagem" :alt="altImagem" />
+    <button @click="apareceEsconde(), segundoMetodo()">Aparece/Esconde</button>
+  </div>
 </template>
 
 <script>
-
-    export default {
-        name: "ImagemComponent",
-        data() {
-            return {
-                altImagem: "Banner da Imagem",
-                urlImagem : "https://blog.ipleaders.in/wp-content/uploads/2021/08/law-firm-1.jpg"
-            }
-        }
+export default {
+  name: "ImagemComponent",
+  data() {
+    return {
+      imageVisivel: false,
+      altImagem: "Banner da Imagem",
+   //   urlImagem:
+       // "https://blog.ipleaders.in/wp-content/uploads/2021/08/law-firm-1.jpg",
+    };
+  },
+  methods: {
+    apareceEsconde() {
+        
+        console.log("FUI CLICADO!");
+        this.imageVisivel =! this.imageVisivel;
+        this.$emit('visibilidadeImg', this.imageVisivel);
+    },
+    segundoMetodo (){
+        setTimeout(function() {
+            console.log("Segundo Metodoo Chamado!")
+        }, 2000);
+        
     }
+  },
+  props: {
+    nomeImagem: String,
+    urlImagem: String
+  },
+  emits:  ["visibilidadeImg"]
 
+}
 </script>
 
 <style scoped>
+button {
+  padding: 12px;
+  font-weight: bold;
+  color: cyan;
+  background-color: azure;
+  border: solid 2px darkcyan;
+  border-radius: 8px;
+  transition: 0.5s;
+  cursor: pointer;
+}
 
-    img {
-        width: 300px;
-        height: auto;
-        border: solid 4px darkcyan;
-        margin: 16px;
-        border-radius: 16px;
-        box-shadow: 2px 2px 10px 2px darkcyan;
-    }
+button:hover {
+    background-color: darkcyan;
+    color: azure;
 
+}
+
+img {
+  width: 300px;
+  height: auto;
+  border: solid 4px darkcyan;
+  margin: 16px;
+  border-radius: 16px;
+  box-shadow: 2px 2px 10px 2px darkcyan;
+}
 </style>
--- vue/cli/src/components/UsuarioComponent.vue
+++ vue/cli/src/components/UsuarioComponent.vue
@@ -3,7 +3,16 @@
           <p>Nome: {{ nome }}</p>
           <p>E-mail: {{ email }}</p>  
 
-           <imagem-component/>
+        <h1 v-show="isFlamengoCampeao">Flamengo Campeão</h1>
+
+
+           <imagem-component 
+           @visibilidadeImg="exigirMsgFlamengo"
+           nomeImagem="Imagem Teste"
+           urlImagem="https://th.bing.com/th/id/OIP.DH_XHziuOGLfjtKKiZUKQwHaEo?rs=1&pid=ImgDetMain" />
+
+           <imagem-component nomeImagem="Imagem Teste"
+           urlImagem="https://th.bing.com/th/id/R.890f25e991b8f73861172bb9ec44322c?rik=IaZJ03oTjlWe9Q&pid=ImgRaw&r=0"/>
 
     </div>
 </template>
@@ -22,7 +31,18 @@ import ImagemComponent from './ImagemComponent.vue'
         data(){
             return {
                 nome : "Fernando",
-                email : "fernando.dias@ifb.edu.br"
+                email : "fernando.dias@ifb.edu.br",
+                isFlamengoCampeao: false
+            }
+        },
+        methods: {
+            exigirMsgFlamengo(isImagemVisivel) {
+                console.log("ExibirMsgFlamengo sendo chamado")
+                if(isImagemVisivel ==true){
+                    this.isFlamengoCampeao = true;
+                }else{
+                    this.isFlamengoCampeao = false;
+                }
             }
         }
     }
