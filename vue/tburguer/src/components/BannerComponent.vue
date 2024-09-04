3f5d93d2bf0bc59467186122b010150f81a43b26
Lucas Gabriel Ribeiro Costa <lucas.costa@isgsa.com.br>
2024-09-04 20:16:31 -0300

    Solucao tburguer

--- vue/tburguer/src/components/BannerComponent.vue
+++ vue/tburguer/src/components/BannerComponent.vue
@@ -8,31 +8,28 @@
     export default {
         name: "BannerComponent"
     }
-
 </script>
-
 
 <style scoped>
 
-  #main-banner {
-    background-image: url("/public/img/banner.jpeg");
-    background-position: 0;
-    background-size: cover;
-    height: 500px;
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
-  }
+    #main-banner {
+        background-image: url("/public/img/banner.jpeg");
+        background-position: 0;
+        background-size: cover;
+        height: 500px;
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+    }
 
-  #main-banner h1 {
-    font-size: 40px;
-    color: antiquewhite;
-    text-align: center;
-    background: #222;
-    padding: 25px;
-    border-radius: 100px 0px 0px 100px;
-  }
-
+    #main-banner h1 {
+        font-size: 40px;
+        color: antiquewhite;
+        text-align: center;
+        background: #222222a9;
+        padding: 25px;
+        border-radius: 100px 0px 0px 100px;
+    }
 
 
 </style>
